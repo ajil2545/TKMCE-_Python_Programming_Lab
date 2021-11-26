d24a061a82b131ab40cf88ee30d5f997d54f353e
ajil2545 <ajilkunjumon2000@gmail.com>
2021-11-26 21:06:21 +0530

    commit

--- Course Outcome 1/20_evenlist.py
+++
@@ -1,6 +0,0 @@
-l=[1,2,3,4,5,6,7,8,9]
-evenlist = [] 
-for i in l:
-    if (i % 2 == 0):
-        evenlist.append(i)
-print("Even lists:", evenlist)
---
+++ Course Outcome 1/20_oddlist.py
@@ -0,0 +1,6 @@
+l=[1,2,3,4,5,6,7,8,9]
+oddlist = [] 
+for i in l:
+    if (i % 2 != 0):
+        oddlist.append(i)
+print("Even lists:", oddlist)
